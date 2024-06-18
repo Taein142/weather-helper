13984bedd5611da0270d50e936399ea2a226b8b6
Taein142 <xodlsdldy@naver.com>
2024-06-18 18:32:22 +0900

    프로젝트 시작

---
+++ src/main/java/com/icia/weatherhelper/WeatherHelperApplication.java
@@ -0,0 +1,13 @@
+package com.icia.weatherhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/icia/weatherhelper/WeatherHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.weatherhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
