d0b341d9f4bde34d03a1b676bec2198580975373
Taein142 <xodlsdldy@naver.com>
2024-07-04 14:51:24 +0900

    시큐리티 및 오류땜에 주석처리 함

--- src/main/java/com/icia/weatherhelper/controller/SignController.java
+++ src/main/java/com/icia/weatherhelper/controller/SignController.java
@@ -1,28 +1,28 @@
-package com.icia.weatherhelper.controller;
-
-import com.icia.weatherhelper.service.SignService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.io.IOException;
-import java.text.ParseException;
-
-@Controller
-@Slf4j
-public class SignController {
-    @Autowired
-    SignService signService;
-
-    @GetMapping("sign-up")
-    public String signUp(){
-        log.info("signIp()");
-
-        return "signUp";
-    }
-}
+//package com.icia.weatherhelper.controller;
+//
+//import com.icia.weatherhelper.service.SignService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//
+//import java.io.IOException;
+//import java.text.ParseException;
+//
+//@Slf4j
+//@Controller
+//public class SignController {
+//    @Autowired
+//    SignService signService;
+//
+//    @GetMapping("sign-up")
+//    public String signUp(){
+//        log.info("signIp()");
+//
+//        return "signUp";
+//    }
+//}
